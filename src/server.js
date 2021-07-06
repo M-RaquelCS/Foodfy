d8b1c91adea9d5e6a02fbe56e69ffb1562b80ea0
M-RaquelCS <M-Raquel@outlook.com.br>
2021-07-06 10:41:52 -0300

    foodfy admin

--- src/server.js
+++ src/server.js
@@ -6,23 +6,24 @@ const dataRecipes = require('./data')
 
 server.use(express.static('public'))
 server.use(express.static('src/styles'))
-server.use(express.static('src/styles/pages'))
+server.use(express.static('src/styles/public'))
+server.use(express.static('src/styles/admin'))
 server.use(express.static('src/styles/components'))
 
 server.set('view engine', 'njk')
 
-nunjucks.configure('src/pages', {
+nunjucks.configure('src/views', {
     express:server,
     noCache: true,
     autoescape: false
 })
-
+/* rotas do cliente */
 server.get('/', function(req, res){
     return res.render('home', { dataRecipes })
 })
 server.get('/recipes', function(req, res){
     return res.render('recipes', { dataRecipes })
-}) 
+})
 server.get('/details', function(req, res){
     const recipes = [...dataRecipes];
     const id = req.query.id;
@@ -34,3 +35,8 @@ server.get('/about', function(req, res){
 server.listen(5000, function(){
     console.log('server')
 })
+/* rotas do administrador */
+server.get('/admin/recipes', function(req,res){
+    res.render('homeAdmin', {dataRecipes})
+})
+/* rotas do administrador */
